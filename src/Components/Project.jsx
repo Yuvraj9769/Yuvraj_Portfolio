78645a6ea5c84bbead82d78d93dc4d74f41a5628
Yuvraj9769 <130125820+Yuvraj9769@users.noreply.github.com>
2024-03-29 22:11:42 +0530

    Update Project.jsx

--- src/Components/Project.jsx
+++ src/Components/Project.jsx
@@ -1,106 +1,92 @@
-import React, { useEffect } from "react";
-import gsap from "gsap";
-import { ScrollTrigger } from "gsap/ScrollTrigger";
-import { Link } from "react-router-dom";
-gsap.registerPlugin(ScrollTrigger);
+import React from "react";
+import { Link, NavLink } from "react-router-dom";
+import { ImCancelCircle } from "react-icons/im";
 
-const Project = () => {
-  useEffect(() => {
-    gsap.set(".projul > li", {
-      scale: 0,
-      opacity: 0,
-    });
-
-    gsap.to(".projul li", {
-      opacity: 1,
-      scale: 1,
-      stagger: 0.2,
-      scrollTrigger: {
-        trigger: ".projul",
-        scroller: "body",
-        start: "top center",
-        toggleActions: "restart none none none",
-      },
-    });
-  }, []);
-
+const Sidebar = ({ Siderbarvisible, setSiderbar }) => {
   return (
-    <div className="w-full bg-black px-3 py-2 flex flex-col gap-3 md:gap-5 my-3">
-      <p className="text-red-600 relative text-xl xl:text-2xl text-center before:absolute before:bg-[#dadada] before:h-1 before:w-[35%] before:top-[50%] xl:after:h-[2px] xl:before:h-[2px] before:left-0  after:absolute after:bg-[#dadada] after:h-1 after:w-[35%] after:top-[50%] after:right-0">
-        Projects
-      </p>
-
-      <div className=" w-full h-auto flex flex-col gap-4 md:flex-row">
-        <div className=" flex flex-col gap-2">
-          <h1 className="text-4xl text-slate-50 py-1 font-semibold">
-            Projects : -
-          </h1>
-          <ul className="text-slate-50 projul">
-            <li className="my-3 hover:text-red-600 duration-300  cursor-pointer whitespace-normal break-words">
-              <Link to="https://github.com/Yuvraj9769/Amazon_clone-using-Tailwind-CSS">
-                https://github.com/Yuvraj9769/Amazon_clone-using-Tailwind-CSS
-              </Link>
-            </li>
-            <li className="my-3 hover:text-red-600 duration-300  cursor-pointer whitespace-normal break-words">
-              <Link to="https://github.com/Yuvraj9769/Spotify_Clone">
-                https://github.com/Yuvraj9769/Spotify_Clone
-              </Link>
-            </li>
-            <li className="my-3 hover:text-red-600 duration-300  cursor-pointer whitespace-normal break-words">
-              <Link to="https://github.com/Yuvraj9769/Flipkart-Clone-Using-Html-Tailwind-css-and-Javascript/blob/main/dist/flipkart.js">
-                https://github.com/Yuvraj9769/Flipkart-Clone-Using-Html-Tailwind-css-and-Javascript/blob/main/dist/flipkart.js
-              </Link>
-            </li>
-            <li className="my-3 hover:text-red-600 duration-300  cursor-pointer whitespace-normal break-words">
-              <Link to="https://github.com/Yuvraj9769/Wow-clone">
-                https://github.com/Yuvraj9769/Wow-clone
-              </Link>
-            </li>
-            <li className="my-3 hover:text-red-600 duration-300  cursor-pointer whitespace-normal break-words">
-              <Link to="https://github.com/Yuvraj9769/Netflix-clone-reactjs">
-                https://github.com/Yuvraj9769/Netflix-clone-reactjs
-              </Link>
-            </li>
-            <li className="my-3 hover:text-red-600 duration-300  cursor-pointer whitespace-normal break-words">
-              <Link to="https://github.com/Yuvraj9769/Landing_page">
-                https://github.com/Yuvraj9769/Landing_page
-              </Link>
-            </li>
-            <li className="my-3 hover:text-red-600 duration-300  cursor-pointer whitespace-normal break-words">
-              <Link to="https://github.com/Yuvraj9769/Portfolio">
-                https://github.com/Yuvraj9769/Portfolio
-              </Link>
-            </li>
-          </ul>
-        </div>
-        <div className="flex flex-col gap-2 text-slate-50">
-          <h1 className="text-4xl py-1 font-semibold">Live Projects : -</h1>
-          <ul className="projul">
-            <li className="my-3 hover:text-green-600 duration-300  cursor-pointer whitespace-normal break-words">
-              <Link to="https://letsspotify.freewebhostmost.com/">
-                https://letsspotify.freewebhostmost.com/
-              </Link>
-            </li>
-            <li className="my-3 hover:text-green-600 duration-300  cursor-pointer whitespace-normal break-words">
-              <Link to="https://flipkartshopping.netlify.app/">
-                https://flipkartshopping.netlify.app/
-              </Link>
-            </li>
-            <li className="my-3 hover:text-green-600 duration-300  cursor-pointer whitespace-normal break-words">
-              <Link to="https://yuvraj9769.github.io/Wow-clone/">
-                https://yuvraj9769.github.io/Wow-clone/
-              </Link>
-            </li>
-            <li className="my-3 hover:text-green-600 duration-300  cursor-pointer whitespace-normal break-words">
-              <Link to="https://yuvraj9769.github.io/Landing_page/">
-                https://yuvraj9769.github.io/Landing_page/
-              </Link>
-            </li>
-          </ul>
+    <div
+      className={`siderbar flex flex-col gap-[35px] md:hidden bg-black transition-left  duration-1000 text-2xl absolute top-0 h-screen w-screen ${
+        Siderbarvisible ? "left-[0%] z-10 fixed" : "left-[-110%]"
+      }`}
+    >
+      <div className="px-4 py-3 flex justify-center items-center">
+        <div className="flex justify-between items-center w-full border-b border-[#dadada] pb-6">
+          <h1 className="text-slate-50 text-4xl font-semibold">Yuvraj</h1>
+          <p
+            className="text-4xl text-red-600"
+            onClick={() => setSiderbar(!Siderbarvisible)}
+          >
+            <ImCancelCircle />
+          </p>
         </div>
       </div>
+      <ul className="flex px-[1.2rem] sm:flex-row w-full min-h-[300px] flex-col text-slate-50 justify-between items-start text-[22px]">
+        <li>
+          <NavLink
+            to="/"
+            className={(e) => {
+              return e.isActive ? "text-red-600" : " ";
+            }}
+            onClick={() => setSiderbar(!Siderbarvisible)}
+          >
+            Home
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to="/Aboutme"
+            className={(e) => {
+              return e.isActive ? "text-red-600" : " ";
+            }}
+            onClick={() => setSiderbar(!Siderbarvisible)}
+          >
+            About Me
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to="/Project"
+            className={(e) => {
+              return e.isActive ? "text-red-600" : " ";
+            }}
+            onClick={() => setSiderbar(!Siderbarvisible)}
+          >
+            Projects
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to="/Contact"
+            className={(e) => {
+              return e.isActive ? "text-red-600" : " ";
+            }}
+            onClick={() => setSiderbar(!Siderbarvisible)}
+          >
+            Contact
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to="/Skills"
+            className={(e) => {
+              return e.isActive ? "text-red-600" : " ";
+            }}
+            onClick={() => setSiderbar(!Siderbarvisible)}
+          >
+            Skills
+          </NavLink>
+        </li>
+        <button
+          onClick={() => setSiderbar(!Siderbarvisible)}
+          className="border border-[#dadada] rounded px-4 py-2 transition-all hover:bg-slate-50 hover:text-red-600 duration-700"
+        >
+          <Link to="https://drive.google.com/file/d/1GeKEjpZGwUxFrhPKSRxxTloUCRkdUdJf/view?usp=sharing">
+            Resume/CV
+          </Link>
+        </button>
+      </ul>
     </div>
   );
 };
 
-export default Project;
+export default Sidebar;

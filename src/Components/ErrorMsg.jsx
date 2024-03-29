fcab81c03d6cea09dc5150316cbad175a8c6f3f9
Yuvraj9769 <130125820+Yuvraj9769@users.noreply.github.com>
2024-03-29 22:09:34 +0530

    Update ErrorMsg.jsx

--- src/Components/ErrorMsg.jsx
+++ src/Components/ErrorMsg.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const ErrorMsg = () => {
   return (
-    <h1 className="text-black text-6xl font-semibold">
+    <h1 className="text-slate-50 text-6xl font-semibold">
       Sorry Page Not Found 404
     </h1>
   );

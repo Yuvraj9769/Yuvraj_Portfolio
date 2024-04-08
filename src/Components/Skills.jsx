c951fc90b6b8ab7b818f56be160d758ae5fed208
Yuvraj9769 <130125820+Yuvraj9769@users.noreply.github.com>
2024-04-08 22:15:20 +0530

    Update Skills.jsx

--- src/Components/Skills.jsx
+++ src/Components/Skills.jsx
@@ -8,7 +8,7 @@ const Skills = () => {
       <p className="text-red-600  relative text-xl text-center before:absolute xl:text-2xl before:bg-[#dadada] before:h-1 before:w-[35%] before:top-[50%] before:left-0  after:absolute after:bg-[#dadada] after:h-1 after:w-[35%] after:top-[50%] after:right-0 xl:after:h-[2px] xl:before:h-[2px]">
         Skills
       </p>
-      <div className="w-full flex flex-col md:flex-row md:flex-wrap gap-6 items-center md:justify-center py-3 px-2 ">
+      <div className="w-full flex flex-col md:flex-row md:flex-wrap gap-6 xl:gap-8 2xl:gap-10 items-center md:justify-center py-3 px-2 ">
         {skilldataInfo.map((e) => (
           <div
             className="px-4 py-3 rounded-lg shadow-md shadow-gray-700 flex flex-col items-center justify-between  border hover:cursor-pointer hover:scale-95 duration-500 w-[280px] h-[465px] xl:gap-5 border-[#dadada] lg:py-1 lg:h-[430px]"
